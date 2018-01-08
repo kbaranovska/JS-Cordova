dea7749c4fb5742b084c13a2a217ff1a4a1d16f6
Kateryna Baranovska <kateryna.baranovska@D2L.com>
2018-01-09 00:20:53 +0200

    - added mobile application for previous work of calculator - add sms text plugin

---
+++ www/js/sms.js
@@ -0,0 +1,23 @@
+var app = {
+    sendSms: function() {
+        var number = document.getElementById('numberTxt').value.toString(); /* iOS: ensure number is actually a string */
+        var message = document.getElementById('messageTxt').value;
+        alert("number=" + number + ", message= " + message);
+
+        //CONFIGURATION
+        var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
+            },
+            ios: {
+                intent: 'INTENT'
+            }
+        };
+
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
+        sms.send(number, message, options, success, error);
+    }
+};
